7161f353ad24b4980060ae219d4dbd45a81dce7d
huydatisme <100422956+huydatisme@users.noreply.github.com>
2024-10-23 00:15:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  // Export static HTML files to use with GitHub Pages
-  output: 'export',
+  // Enable experimental app directory
+  experimental: {
+    appDir: true, // Kích hoạt cấu trúc thư mục "app"
+  },
 
-  // Optional: If you need to set a base path for GitHub Pages
-  basePath: '/<REPO_NAME>', // Replace with the name of your repository
+  // If you need a base path for GitHub Pages
+  basePath: 'https://github.com/huydatisme/Website-in', // Replace with the name of your repository
+
+  // If you're using "standalone" output (optional, but good for optimization)
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
