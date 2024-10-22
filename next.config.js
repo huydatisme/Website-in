1cac8a47cedccdf8ea948891a8dc2861cea9570b
huydatisme <100422956+huydatisme@users.noreply.github.com>
2024-10-22 17:26:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'https://github.com/huydatisme/Website-in/';
+module.exports = {
+ 
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  
   output: 'export',
-  distDir:'dist',
-  experimental: {
-    appDir: true,
+  images: {
+    unoptimized: true, // Disable image optimization as GitHub Pages doesn't support it
   },
 };
-
-module.exports = nextConfig;
