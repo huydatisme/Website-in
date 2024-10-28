6164eaccc1737f8c0fc7bab6ac04ffe03d93f544
huydatisme <datprono28@gmail.com>
2024-10-28 19:37:59 +0700

    update component

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  // Enable experimental app directory
   experimental: {
-    appDir: true, // Kích hoạt cấu trúc thư mục "app"
+    appDir: true,
   },
-
-  // If you need a base path for GitHub Pages
-  basePath: 'https://huydatisme.github.io/Website-in/', // Replace with the name of your repository
-
-  // If you're using "standalone" output (optional, but good for optimization)
-  output: 'standalone',
 };
-
 module.exports = nextConfig;
